afbd32cd540da9a96f3e726316fc669ba2e0f3ef
Ayush Saurav <theasaurav@gmail.com>
2021-09-11 19:50:13 +0530

    Global Time zone

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const z = momnt.tz.setDefault('America/Los_Angeles');
 let targetTZ;
 
 if(process.argv.length != 3) {
-    console.log("Wrong Input <script-file> <timezone>", process.argv);
+    console.log("Wrong Input: Please use - node <script-file> <timezone>");
     process.exit(1);
 } else {
     targetTZ = process.argv[2];
